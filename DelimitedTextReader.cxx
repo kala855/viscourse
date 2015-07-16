6c13c02a195fe92eb0a7251b495932e18171525f
John H. Osorio Ríos <john@sirius.utp.edu.co>
2015-07-16 00:14:31 -0500

    Commit inicial. Se tiene una versión funcional de visualización. Se pueden graficar barras de datos presentes en un archivo csv. Hay problemas con el escalado de los datos.

---
+++ DelimitedTextReader.cxx
@@ -0,0 +1,91 @@
+
+#include <vtkVersion.h>
+#include <vtkSmartPointer.h>
+#include <vtkProperty.h>
+#include <vtkTable.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkVertexGlyphFilter.h>
+#include <vtkContextView.h>
+#include <vtkContextScene.h>
+#include <vtkPlot.h>
+#include <vtkChartXY.h>
+#include <vtkAxis.h>
+#include <vtkDelimitedTextReader.h>
+
+#define VTK_CREATE(type, name) \
+    vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+
+int main(int argc, char* argv[])
+{
+  // Verify input arguments
+  if(argc != 2)
+    {
+    std::cout << "Usage: " << argv[0]
+              << " Filename(.csv)" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::string inputFilename = argv[1];
+
+
+  VTK_CREATE(vtkContextView,view);
+  view->GetRenderer()->SetBackground(1.0,1.0,1.0);
+  view->GetRenderWindow()->SetSize(400,300);
+  VTK_CREATE(vtkChartXY, chart);
+  view->GetScene()->AddItem(chart);
+
+  VTK_CREATE(vtkDelimitedTextReader,reader);
+  reader->SetFileName(inputFilename.c_str());
+  reader->DetectNumericColumnsOn();
+  reader->SetFieldDelimiterCharacters(",");
+  reader->SetHaveHeaders(true);
+  reader->Update();
+
+  vtkTable* table = reader->GetOutput();
+
+  std::cout << "Table has " << table->GetNumberOfRows()
+            << " rows." << std::endl;
+  std::cout << "Table has " << table->GetNumberOfColumns()
+            << " columns." << std::endl;
+
+  for(vtkIdType i = 0; i < table->GetNumberOfRows(); i++)
+    {
+    std::cout <<"Data:" << (table->GetValue(i,0)).ToDouble()
+              <<" Penicillin:" << (table->GetValue(i,1)).ToDouble()
+              <<" Streptomycin:" << (table->GetValue(i,2)).ToDouble()
+              <<" Neomycin:" << (table->GetValue(i,3)).ToDouble()
+              <<" Gram Staining:" << (table->GetValue(i,4)).ToDouble()
+              << endl;
+    }
+
+  vtkPlot *line = 0;
+
+  VTK_CREATE(vtkAxis, ejey);
+
+  ejey = chart->GetAxis(vtkAxis::LEFT);
+  ejey->SetLogScale(false);
+  ejey->Update();
+
+  line = chart->AddPlot(vtkChart::BAR);
+  line->SetInputData(table,0,1);
+  line->SetColor(255,0,0,255);
+
+  line = chart->AddPlot(vtkChart::BAR);
+  line->SetInputData(table,0,2);
+  line->SetColor(0,255,0,255);
+
+  line = chart->AddPlot(vtkChart::BAR);
+  line->SetInputData(table,0,3);
+  line->SetColor(0,255,255,255);
+
+
+
+  view->GetRenderWindow()->SetMultiSamples(0);
+  view->GetInteractor()->Initialize();
+  view->GetInteractor()->Start();
+
+  return EXIT_SUCCESS;
+}
